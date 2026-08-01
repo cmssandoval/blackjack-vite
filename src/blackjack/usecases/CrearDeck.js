112c561c6234c81911a7d464f05ee7f659e67962
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-08-01 11:46:25 -0400

    Docs: Documentar función JSDoc Comments y agregar errores personalizados

--- src/blackjack/usecases/CrearDeck.js
+++ src/blackjack/usecases/CrearDeck.js
@@ -1,20 +1,29 @@
 import _ from 'underscore';
 
-// Función para crear y barajar el deck
-export const crearDeck = ( palos, figuras) => {
+/**
+ *  Función para crear y barajar el deck
+ * @param {Array<String>} palos  Ejemplo: ['C', 'D', 'H', 'S']
+ * @param {Array<String>} figuras Ejemplo: ['A', 'J', 'Q', 'K']
+ * @returns {Array<String>} retorna un nuevo deck de cartas barajado
+ */
+export const crearDeck = ( palos, figuras ) => {
 
-  let deck = [];
-  
-  for (let i = 2; i <= 10; i++) {
+    if ( !palos || palos.length === 0 ) throw new Error('palos es obligatorio como un arreglo de string');
+
+    if ( !figuras || figuras.length === 0 ) throw new Error('figuras es obligatorio como un arreglo de string');
+
+    let deck = [];
+
+    for (let i = 2; i <= 10; i++) {
+        for (let palo of palos) {
+            deck.push(i + palo)
+        }
+    }
+
     for (let palo of palos) {
-      deck.push(i + palo)
+        for (let figura of figuras) {
+            deck.push(figura + palo);
+        }
     }
-  }
-
-  for (let palo of palos) {
-    for (let figura of figuras) {
-      deck.push(figura + palo);
-    }
-  }
-  return _.shuffle(deck);
+    return _.shuffle(deck);
 };
